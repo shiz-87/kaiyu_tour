2567738681e54ef65b0b7b8e21e2c2700eabc31d
shiz__87 <shizukayamamoto87@gmail.com>
2026-01-29 18:04:36 +0900

    feat: activityエリアのHTML/CSS・Sliderの実装

--- js/main.js
+++ js/main.js
@@ -30,3 +30,24 @@ $(function () {
     });
   });
 });
+
+// activityセクションのSlider
+$(function () {
+  $("#activity .slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    centerMode: true,
+    slidesToShow: 3,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});

e9713dd74531847498ca9764971625432ae9747f
shiz__87 <shizukayamamoto87@gmail.com>
2026-01-29 16:57:31 +0900

    feat: introduction sectionのテキストの表示に関するアニメーションの実装

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// ハンバーガーメニューの開閉
+
 $(function () {
   $(".hamburger").on("click", function () {
     $(this).toggleClass("active");
@@ -7,5 +9,24 @@ $(function () {
   $(".navi a").on("click", function () {
     $(".hamburger").removeClass("active");
     $(".navi").removeClass("active");
+  });
+});
+
+// introductionセクションのアニメーション
+$(function () {
+  $("#introduction .tour1 .btn").on("click", function () {
+    $(this).hide();
+    $("#introduction .tour1 .description").css({
+      height: "auto",
+      overflow: "visible",
+    });
+  });
+
+  $("#introduction .tour2 .btn").on("click", function () {
+    $(this).hide();
+    $("#introduction .tour2 .description").css({
+      height: "auto",
+      overflow: "visible",
+    });
   });
 });
